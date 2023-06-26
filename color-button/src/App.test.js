93756c8eb3a0339e7f834cbe595fa621ea0431b3
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-06-26 21:58:28 +0900

    logRole(role을 log 할수있는 기능) 사용

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { logRoles, render, screen } from "@testing-library/react";
 import App from "./App";
 
 // test("renders learn react link", () => {
@@ -10,7 +10,8 @@ import App from "./App";
 // });
 
 test("button has correct initial color", () => {
-  render(<App />);
+  const { container } = render(<App />);
+  logRoles(container);
 
   // find an element with a role of button and text of 'Change to blue'
   const colorButton = screen.getByRole("button", { name: "Change to blue" });
