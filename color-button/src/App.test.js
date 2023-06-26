b362b73a065dcd935cca321860a86b32a949f3cc
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-06-26 21:52:10 +0900

    초기 상태 red flag 처리

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+// test("renders learn react link", () => {
+//   render(<App />);
+//   // const linkElement = screen.getByText(/learn React/i);
+//   const linkElement = screen.getByRole("link", { name: /learn React/i });
+//   expect(linkElement).toBeInTheDocument();
+//   // throw new Error("test fail");
+// });
+
+test("button has correct initial color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn React/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
 });
+
+test("button turns blue when clicked", () => {});
