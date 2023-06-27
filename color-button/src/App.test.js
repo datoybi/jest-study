f1e576858727f527fab5b79af9b7436bf06ce7d2
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-06-27 15:45:59 +0900

    test and code button click

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,4 +1,4 @@
-import { logRoles, render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 // test("renders learn react link", () => {
@@ -9,15 +9,26 @@ import App from "./App";
 //   // throw new Error("test fail");
 // });
 
-test("button has correct initial color", () => {
-  const { container } = render(<App />);
-  logRoles(container);
+test("button has correct initial color, and updates when clicked", () => {
+  render(<App />);
 
   // find an element with a role of button and text of 'Change to blue'
   const colorButton = screen.getByRole("button", { name: "Change to blue" });
 
   // expect the background color to be red
   expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  // expect the button text to be Change to red
+  expect(colorButton).toHaveTextContent("Change to red");
 });
 
-test("button turns blue when clicked", () => {});
+// test("button turns blue when clicked", () => {
+//   render(<App />);
+//   const colorButton = screen.getByRole("button", { name: "Change to blue" });
+// });
