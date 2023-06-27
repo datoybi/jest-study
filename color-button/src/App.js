16f8114145e62d03d903207f1919ea614ef95980
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-06-27 17:33:54 +0900

    unit test green flag 테스트

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import { useState } from "react";
 
-export function replaceCamelWithSpaces(colorName) {}
+export function replaceCamelWithSpaces(colorName) {
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
+}
 
 function App() {
   const [buttonColor, setButtonColor] = useState("red");
