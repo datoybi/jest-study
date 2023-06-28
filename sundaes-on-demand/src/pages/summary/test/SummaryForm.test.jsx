e6ec01ed8fc5cbcb8e1e7569648df325ecea2c96
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-06-28 23:00:22 +0900

    label 호버시 popover 보이는 테스트 구현

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -28,3 +28,27 @@ test("Checkbox enabled button on first click and disabled on second click", asyn
   await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
 });
+
+test("popover responds to hover", async () => {
+  const user = userEvent.setup();
+
+  render(<SummaryForm />);
+
+  // popover starts out hidden
+  // queryBy를 이용하여 존재하지 않으면 null 반환하여 존재하지 않음을 테스트
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears on mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  // 존재하길 배라니까 getby 사용
+  expect(popover).toBeInTheDocument();
+
+  // popover disappears when we mouse out
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
+});
