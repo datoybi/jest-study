eef9b93d3380331d063aa829ecb727bdfbd1543c
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-06-28 22:28:42 +0900

    fix: fireEvent를 userEvent로 마이그레이션

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,6 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test("initial conditions", () => {
   render(<SummaryForm />);
@@ -12,16 +13,18 @@ test("initial conditions", () => {
   expect(confirmButton).toBeDisabled();
 });
 
-test("Checkbox enabled button on first click and disabled on second click", () => {
+test("Checkbox enabled button on first click and disabled on second click", async () => {
+  const user = userEvent.setup();
+
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
     name: /terms and Conditions/i,
   });
   const confirmButton = screen.getByRole("button", { name: /Confirm order/ });
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
 });
