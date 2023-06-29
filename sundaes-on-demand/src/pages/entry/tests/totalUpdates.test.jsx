63a4ca89636588750f400841436e92f0df257da2
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-06-29 15:38:23 +0900

    feat: 합계와 관련된 context 구현

--- sundaes-on-demand/src/pages/entry/tests/totalUpdates.test.jsx
+++ sundaes-on-demand/src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,10 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Options from "../Options";
+import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
 
 test("update scoop subtotal when scoops change", async () => {
   const user = userEvent.setup();
-  render(<Options optionType="scoops" />);
+  render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider });
 
   // make sure total starts out at $0.00
   // 보일까 안보일까?? 동기일까 비동기일까??를 생각해봐야함
