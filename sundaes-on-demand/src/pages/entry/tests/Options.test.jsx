78784c19aa00b9b0dbea3b4207f96babbd605132
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-06-29 11:04:25 +0900

    feat: 서버에서 scoop option 이미지 경로 가져오기 코드 작성 및 테스트 작성

---
+++ sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+
+import Options from "../Options";
+
+test("displays image for each scoop option from server", async () => {
+  render(<Options optionType="scoops" />);
+
+  // find images
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+  // findAllByRole을 사용해야한다! 비동기로 img를 가져오니까!!(중요)
+  expect(scoopImages).toHaveLength(2);
+
+  // confirm alt text of images
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
